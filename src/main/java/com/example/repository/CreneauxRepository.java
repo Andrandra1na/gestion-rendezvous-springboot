330fef8242c445605b11e14560eeb4141d636667
Andrandraina <se20200047@gmail.com>
2025-04-27 21:46:03 +0300

    🗃 Ajout des interfaces Repository JPA pour toutes les entités

--- src/main/java/com/example/repository/CreneauxRepository.java
+++ src/main/java/com/example/repository/CreneauxRepository.java
@@ -3,5 +3,9 @@ package com.example.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.entity.Creneaux;
 
+/**
+ * CreneauxRepository is an interface that extends JpaRepository to provide CRUD operations for Creneaux entities.
+ * It allows for easy interaction with the database without the need for boilerplate code.
+ */
 public interface CreneauxRepository extends JpaRepository<Creneaux, Long> {
 }
